67fd2fdc6b39ecaf6907fa8f9a9239dda37fa6d4
mmeacham <maxwmeacham@gmail.com>
2020-02-23 21:27:32 -0500

    Fixed refresh error
    error removed, info displaying in network tab

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 $(() => {
-  handleGetBook = (bookTitle) => {
-    let xhttp = new XMLHttpRequest();
 
+  $("#book-info").click(function(e){
+    // Referenced URL: https://stackoverflow.com/questions/27759380/how-to-stop-refreshing-page-after-ajax-call/40650166
+    // TLDR; only neccessary if something above is listening to the (default-)event too
+    e.preventDefault();
+
+    let xhttp = new XMLHttpRequest();
+    // Referenced URL: https://www.w3schools.com/js/js_json_http.asp
     xhttp.onreadystatechange = function() {
       if ((this.readystate == 4) && (this.status == 200)) {
-        document.getElementById('book-title').InnerHTML = this.responseText;
+        let response = JSON.parse(this.responseText);
+        console.log('Success');
+        console.log(response.items[0]);
       }
     };
 
-    xhttp.open("GET", 'https://www.googleapis.com/books/v1/volumes?q=' + bookTitle, true);
+    let searchString = $('#book-title').val();
+    console.log(searchString);
+
+    xhttp.open("GET", 'https://www.googleapis.com/books/v1/volumes?q=' + searchString, true);
     xhttp.send();
 
     console.log('Created request');
-  }
+  })
 });
